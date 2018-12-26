eeccb5caa466616963ca87e3c6029536fdc4f00d
Maen <artimym@gmail.com>
2018-12-26 11:57:24 -0400

    send only yesterday's data then zip them

--- run-sender.sh
+++ run-sender.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 LOGFILE=sent.log
-#for f in $FILES
-for f in $1
+# Send all files created yeserday.
+FILES=/home/pi/sensordata/sensordata-$(date -d "yesterday" '+%Y-%m-%d')*
+HOST=gvm@192.168.2.15
+RFOLDER=/home/gvm/sensordata/
+
+for f in $FILES
 do
-  echo "Sending file $f to host $2, fodler $3"
+  echo "Sending file $f to host $HOST, fodler $RFOLDER"
   # take action on each file. $f store current file name
-  scp -i ~/.ssh/id_rsa -C -r $f $2:$3 
+  scp -i ~/.ssh/id_rsa -C -r $f $HOST:$RFOLDER 
   status=$?
   if test $status -eq 0
   then
       { { date; echo ": File $f is sent successfully."; } | tr "\n" " "; echo ""; } >> $LOGFILE
+      gzip $f
   else
       { { date; echo ": File $f could not be sent. Error: $status."; } | tr "\n" " "; echo ""; }  >> $LOGFILE
   fi
